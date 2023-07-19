cb924a481d3dd7af21a339f09a5af8cbcaf53b53
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2023-07-19 07:22:30 -0600

    24-LayoutPages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bases_web/router/router_generator.dart';
+import 'package:bases_web/ui/layout/main_layout_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -8,11 +9,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'RutasApp',
       initialRoute: '/stateful',
       onGenerateRoute: RouterGenerator.generateRoute,
+      builder: (_, child) {
+        return MainLayoutPage(child: child ?? Container());
+      },
     );
   }
 }
--- lib/ui/layout/main_layout_page.dart
+++ lib/ui/layout/main_layout_page.dart
@@ -2,6 +2,11 @@ import 'package:bases_web/ui/shared/custom_app_menu.dart';
 import 'package:flutter/material.dart';
 
 class MainLayoutPage extends StatelessWidget {
+
+  final Widget child;
+
+  const MainLayoutPage({super.key, required this.child});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,7 +15,7 @@ class MainLayoutPage extends StatelessWidget {
           children: [
             CustomAppMenu(),
             Spacer(),
-            // TODO view
+            Expanded(child: child),
             Spacer(),
           ],
         ),
