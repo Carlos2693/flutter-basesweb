fc1e261510459ac75c61365efbf1d0d6e21a60d4
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2023-07-19 08:25:22 -0600

    27-Menu responsivo

--- lib/ui/shared/custom_app_menu.dart
+++ lib/ui/shared/custom_app_menu.dart
@@ -6,6 +6,19 @@ import 'package:flutter/material.dart';
 class CustomAppMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (_, constraints) =>
+        (constraints.maxWidth > 520)
+          ? _TableDesktopMenu()
+          : _MobileMenu()
+    );
+  }
+}
+
+class _TableDesktopMenu extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(
         horizontal: 20,
@@ -13,6 +26,45 @@ class CustomAppMenu extends StatelessWidget {
       ),
       width: double.infinity,
       child: Row(
+        children: [
+          CustomFlatButton(
+            text: 'Contador Stateful',
+            // onPressed: () => Navigator.pushNamed(context, '/stateful'),
+            onPressed: () => locator<NavigationService>().navigateTo('/stateful'),
+            color: Colors.black,
+          ),
+          SizedBox(width: 10),
+          CustomFlatButton(
+            text: 'Contador Provider',
+            // onPressed: () => Navigator.pushNamed(context, '/provider'),
+            onPressed: () => locator<NavigationService>().navigateTo('/provider'),
+            color: Colors.black,
+          ),
+          SizedBox(width: 10),
+          CustomFlatButton(
+            text: 'Otra página',
+            // onPressed: () => Navigator.pushNamed(context, '/abc123'),
+            onPressed: () => locator<NavigationService>().navigateTo('/abc123'),
+            color: Colors.black,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _MobileMenu extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 10,
+      ),
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomFlatButton(
             text: 'Contador Stateful',
